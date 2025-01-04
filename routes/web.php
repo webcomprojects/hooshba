9c3e7e3fa372b08f3b5ec656c22f626982a90ed6
LonelyDeev <mahdi77yousefi2015@gmail.com>
2025-01-04 22:17:05 +0330

    روت پاک کردن کش

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,5 +16,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/clear-cache', function () {
+    // پاک کردن کش‌های Laravel
+    Artisan::call('cache:clear');
+    Artisan::call('config:clear');
+    Artisan::call('route:clear');
+    Artisan::call('view:clear');
+    Artisan::call('permission:cache-reset');
+
+    return "تمام کش‌ها با موفقیت پاک شدند!";
 });
 
