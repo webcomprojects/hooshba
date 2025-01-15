8b1e582bd7f2958ffd11d8bd59036e5ba2a6b1e8
webcomprojects <webcom.projects@gmail.com>
2025-01-15 12:45:02 +0330

    add expert register

--- app/Models/UserMeta.php
+++ app/Models/UserMeta.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserMeta extends Model
 {
+    public function getValueAttribute($value)
+    {
+        return unserialize($value);
+    }
+    
     public function user()
     {
         return $this->belongsTo(User::class);
