80a903f5aab4dbf640fe0afdc191b10f027dd6cb
LonelyDeev <mahdi77yousefi2015@gmail.com>
2025-02-03 21:25:40 +0330

    افزودن Api برای ذخیره فرم membership

--- app/Models/JobHistory.php
+++ app/Models/JobHistory.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
 class JobHistory extends Model
 {
-    //
+    use HasUuids;
 }
