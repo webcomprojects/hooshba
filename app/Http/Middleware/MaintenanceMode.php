fcf457de6e054d9cfc46299166abca10f7f72035
LonelyDeev <mahdi77yousefi2015@gmail.com>
2025-03-03 21:44:37 +0330

    تغییرات جدید

---
+++ app/Http/Middleware/MaintenanceMode.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class MaintenanceMode
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+
+        return response()->view('comingsoon.index');
+        //if (!auth()->check() || auth()->user()->role !== 'admin') {
+            //return response()->view('maintenance');
+        //}
+        //return $next($request);
+
+    }
+}
